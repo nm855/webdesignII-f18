c1d055cd78f0cc7fc2b5fca53a97f6d712e87bf2
Nelson <nm855@cabrini.edu>
2018-10-12 10:47:06 -0400

    Initial commit

---
+++ js/header-functions.js
@@ -0,0 +1,16 @@
+// grab an element
+var myElement = document.querySelector("header");
+// construct an instance of Headroom, passing the element
+var headroom  = new Headroom(myElement);
+// initialise
+headroom.init(); 
+
+$.scrollIt({
+  upKey: 38,             // key code to navigate to the next section
+  downKey: 40,           // key code to navigate to the previous section
+  easing: 'linear',      // the easing function for animation
+  scrollTime: 600,       // how long (in ms) the animation takes
+  activeClass: 'active', // class given to the active nav element
+  onPageChange: null,    // function(pageIndex) that is called when page is changed
+  topOffset: 0           // offste (in px) for fixed top navigation
+});
